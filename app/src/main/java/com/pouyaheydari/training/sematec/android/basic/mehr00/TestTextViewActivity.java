0b539731b75525775f8e22c8a5ebbfd1983e03da
Pouya Heydari <pouyaheydary@gmail.com>
2021-10-29 13:16:07 +0330

    removed extra lines in TestTextView'

--- app/src/main/java/com/pouyaheydari/training/sematec/android/basic/mehr00/TestTextViewActivity.java
+++ app/src/main/java/com/pouyaheydari/training/sematec/android/basic/mehr00/TestTextViewActivity.java
@@ -10,9 +10,5 @@ public class TestTextViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_text_view);
-
-
-
-
     }
 }
