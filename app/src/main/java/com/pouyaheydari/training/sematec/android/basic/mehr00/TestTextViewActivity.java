c1bdfa1bbbe3b742dca3a3fc9ec88a3bf092994e
Pouya Heydari <pouyaheydary@gmail.com>
2021-10-29 13:09:48 +0330

    added some lines in testTextView

--- app/src/main/java/com/pouyaheydari/training/sematec/android/basic/mehr00/TestTextViewActivity.java
+++ app/src/main/java/com/pouyaheydari/training/sematec/android/basic/mehr00/TestTextViewActivity.java
@@ -10,5 +10,9 @@ public class TestTextViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_text_view);
+
+
+
+
     }
 }
